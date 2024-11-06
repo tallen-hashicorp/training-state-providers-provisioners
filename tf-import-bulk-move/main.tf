66d9f616c849b51f6e67f6679c4737c4b5495865
Tyler Allen <tyler.allen@hashicorp.com>
2024-11-06 16:43:13 +0000

    Adding in move

--- tf-import-bulk-move/main.tf
+++ tf-import-bulk-move/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
   region = "us-west-2"  # Replace with your preferred region
 }
 
+# Import
+
 # import {
 #  to = aws_s3_bucket.example
 #  id = "tallen-test-import"
@@ -11,3 +13,14 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket = "tallen-massive-test-bucket"  # Unique bucket name
 }
+
+# Moved
+
+# resource "aws_s3_bucket" "new" {
+#   bucket = "tallen-massive-test-bucket"  # Unique bucket name
+# }
+
+# moved {
+#  from = aws_s3_bucket.example
+#  to   = aws_s3_bucket.new
+# }
